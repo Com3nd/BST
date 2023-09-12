59146d245b84f9f998ac1bb887992854417a525b
Mustafa Salman <frostnote2@gmail.com>
2023-09-12 23:38:49 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.left: Optional[TreeNode] = None  # Left is less than
+        self.right: Optional[TreeNode] = None  # right is greater than
+        self.value = value
+
+    def insert(self, value, node: Optional['TreeNode'] = None):
+        if node is None:
+            node = self
+        if not node.left:
+            if value < node.value:
+                node.left = TreeNode(value)
+                return
+        elif value < node.value:
+            self.insert(value, node.left)
+
+        if not node.right:
+            if value > node.value:
+                node.right = TreeNode(value)
+                return
+        elif value > node.value:
+            self.insert(value, node.right)
+
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(self.value)
+        if self.right:
+            self.right.inorder_traversal()
+
+
+if __name__ == '__main__':
+    tree = TreeNode(6)
+    tree.insert(5)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(19)
+    tree.insert(29)
+    tree.insert(11)
+    tree.insert(4)
+    tree.insert(2)
+    tree.inorder_traversal()
+    print()
